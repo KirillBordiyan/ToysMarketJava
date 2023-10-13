3d7c63c8c687a567f1797ceb729d11f575f49ca0
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-10-13 13:27:14 +0300

    market & toy entity upd

---
+++ src/main/java/org/example/model/toy/Toy.java
@@ -0,0 +1,34 @@
+package org.example.model.toy;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Toy implements Comparable<Toy>{
+
+    int id;
+    String name;
+    int dropChance;
+
+    public Toy(int id, String name, int dropChance){
+        this.id = id;
+        this.name = name;
+        this.dropChance = dropChance;
+    }
+
+    @Override
+    public int compareTo(Toy o) {
+        if(this.getDropChance() == o.dropChance){
+            return 0;
+        }
+        return this.dropChance < o.dropChance ? 1: -1;
+    }
+
+    @Override
+    public String toString() {
+        return "Toy:" +
+                " name = " + name +
+                ", dropChance =" + dropChance;
+    }
+}
